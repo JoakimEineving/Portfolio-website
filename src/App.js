339f00b8eae052c2399592d334183ecfc52f9800
joakimeineving <Joakim.eineving@gmail.com>
2022-08-17 15:57:45 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
+import './App.scss';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/container/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './About.scss';
+
+const about = () => {
+  return (
+    <div>about</div>
+  )
+}
+
+export default about
