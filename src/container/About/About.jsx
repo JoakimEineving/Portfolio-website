e0ae31d3e7caa47bb04d4c6ff62f3b1d2f68730d
JoakimEineving <Joakim.eineving@gmail.com>
2022-08-19 16:16:04 +0200

    skills section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,18 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
-import './About.scss';
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../../components/AnimatedLetters'
+import './About.scss'
 
-const about = () => {
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
   return (
     <>
-      {/* <h2 className='head-text'>
-        Hello
-        <span>hello2</span>
-        hello3
-        <span>hello4</span>
-      </h2> */}
+      <div className="container-about about-page">
+        <div className="text-zone">
+       
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm a very ambitious front-end developer looking for a role in an
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+          <p>
+            I'm quiet confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          </p>
+          <p>
+            If I need to define myself in one sentence that would be a family
+            person, father of a beautiful daughter, a sports fanatic,
+            photography enthusiast, and tech-obsessed!!!
+          </p>
+        </div>
+     
+      </div>
     </>
   )
 }
 
-export default about
+export default About
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
 import { motion} from 'framer-motion';
 
+
 const Header = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = [' ', 'J', 'o', 'a', 'k', 'i', 'm', ',']
@@ -31,7 +32,7 @@ const Header = () => {
       <>
     <section>
     
-    <div className="container home-page">
+    <div className="container-header home-page">
             <div className="text-zone">
                 <h1>
                     <span className={letterClass}>H</span>
@@ -49,13 +50,18 @@ const Header = () => {
                     idx={22} />
                 </h1>
                     <h2>Frontend Developer / Backend Developer / Photographer</h2>
-                    {/* <Link to="/contact" className='flat-button'>CONTACT ME</Link> */}
+                    {/* <Link to="" className='flat-button'>CONTACT ME</Link> */}
 
             </div>
+            <svg class="arrows">
+				<path class="a1" d="M0 0 L30 32 L60 0"></path>
+				<path class="a2" d="M0 20 L30 52 L60 20"></path>
+				<path class="a3" d="M0 40 L30 72 L60 40"></path>
+			</svg>
         </div>
         
         </section>
-        <Loader type="pacman" />
+
         </>
   )
 }
