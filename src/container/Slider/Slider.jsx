343daceecffc859336fd44ecedb90db2473b1234
joakimeineving <Joakim.eineving@gmail.com>
2023-01-13 11:21:06 +0100

    add swiper package

---
+++ src/container/Slider/Slider.jsx
@@ -0,0 +1,36 @@
+
+import "./Slider.scss";
+import "swiper/css/bundle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useRef, useState } from "react";
+import { Pagination } from "swiper";
+
+
+export default function Slider() {
+  return (
+    <div className="container-slider">
+      <h1>Projects</h1>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
+
+
